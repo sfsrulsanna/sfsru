88c9db5bdbc481cb5c68315fa5d94552d2971d26
sfsrulsanna <sfsr.ulsanna@gmail.com>
2026-03-28 22:25:32 +0300

    a3.0.10_18e

--- js/personal-profile/documents/passport.js
+++ js/personal-profile/documents/passport.js
@@ -307,7 +307,7 @@ async function renderBook(passportData) {
     if (qrDiv && passportData.personal_code) {
       qrDiv.innerHTML = '';
       new QRCode(qrDiv, {
-		text = `https://sfsru.vercel.app/epass/view/passport.html?personal_code=${passport.personal_code}`;
+		text = `https://sfsru.vercel.app/epass/view/passport.html?personal_code=${passport.personal_code}`,
         width: 80, height: 80,
         colorDark: '#000', colorLight: '#fff',
         correctLevel: QRCode.CorrectLevel.L
